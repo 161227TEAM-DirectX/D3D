b08528fb0e3fe85af92527f66980f60d15f24326
LHC <ahehfldi007@naver.com>
2017-06-26 01:56:44 +0900

    <이미터 추가>  - 빔 이미터 추가  - 실린더 이미터 추가

---
+++ 팀 프레임워크/dxBeamEmitter.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "dxEmitter.h"
+#include "dxParticle.h"
+
+class dxBeamEmitter : public dxEmitter
+{
+private:
+	tagDxParticleEX*	 _ptcEx;
+	
+	tagDxParticleEX*	_ptcVertex;		//파티클 버텍스
+	
+	DWORD*				_ptcIndex;		//파티클 인덱스
+	
+	tagDxParticleEX*	_drawVertex;
+
+	D3DXVECTOR3 _posCenter;
+
+	dx::transform* _trans;
+
+
+	int _onePtcNum;
+
+public:
+	virtual HRESULT init(string textureFileName, int OneTimePaticleNum = 10, float spawnTime = 0.1f, int totalPaticleNum = 1.0f);
+	virtual void relese();
+	virtual void update();
+	virtual void render();
+	//virtual void reset();
+
+private:
+	void ActiveUpdatePlane(tagDxParticleEX * ptcVertex, DWORD * ptcIndex, vector<tagDxAttribute>::iterator iter, DWORD drawParticleNum);
+
+
+
+public:
+	dxBeamEmitter()
+	{
+		//이미터 초기화
+		this->EmitterInit();
+		//이미터 타입 설정
+		_EmitterType = ET_PLANE;
+
+	};
+	~dxBeamEmitter() {};
+};
+
