1ae45d86e787df061790bb045fcb68453f9ea8b8
김태훈 <xogns3310@naver.com>
2017-07-17 16:57:08 +0900

    몬스터 HP바 문제

--- 팀 프레임워크/xPlayer.cpp
+++ 팀 프레임워크/xPlayer.cpp
@@ -379,12 +379,12 @@ void xPlayer::render()
 	//_edgeTrans->RenderGimozo();
 	//_mountObject->_transform->RenderGimozo();
 
-	/*if (targetMonster != NULL)
+	if (targetMonster != NULL)
 	{
-		FONTMANAGER->fontOut("몬스터HP : " + to_string(targetMonster->getHP()), 300, 300, 0xffffffff);
-		FONTMANAGER->fontOut("공격력 : " + to_string(PLAYERMANAGER->Getatt()), 300, 350, 0xffffffff);
+		//FONTMANAGER->fontOut("몬스터HP : " + to_string(targetMonster->getHP()), 300, 300, 0xffffffff);
+		//FONTMANAGER->fontOut("공격력 : " + to_string(PLAYERMANAGER->Getatt()), 300, 350, 0xffffffff);
 		targetMonster->setIsRender(true);
-	}*/
+	}
 
 	//렌더링은 씬에 렌더오브젝트를 넘겨 처리한다.
 	if (KEYMANAGER->isToggleKey(VK_F7))
@@ -417,7 +417,6 @@ void xPlayer::render()
 
 		_mountObject->_transform->RenderGimozo();
 	}
-
 
 	if (!_isMount && _nowSelectedSkill != SKILL_WHIRLWIND)
 	{
